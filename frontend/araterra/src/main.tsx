1c114474f62e64d7dace6667d3d7446f692259ef
Michel Pereira <michelpereira4410@gmail.com>
2026-05-28 23:02:51 -0300

    feat: add Vlibras

--- frontend/araterra/src/main.tsx
+++ frontend/araterra/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import VLibras from "@djpfs/react-vlibras";
 import App from "./App";
 import "leaflet/dist/leaflet.css";
 import "./styles/global.css";
@@ -13,8 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <VLibras />
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
